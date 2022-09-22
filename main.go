0b1ca065dfcb653c4e8bbe199df3251f1d47f59a
Seyit Ahmet Gökçe <seyitahmetgkc@gmail.com>
2022-09-22 13:01:32 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,24 +1,20 @@
 package main
 
 import (
-	/*"net/http"
+	"net/http"
 
 	"github.com/seyitahmetgkc/gowallval/data"
 	. "github.com/seyitahmetgkc/gowallval/handlers"
 
-	"github.com/gorilla/mux"*/
+	"github.com/gorilla/mux"
 	"github.com/seyitahmetgkc/gowallval/validators/band_validator"
 )
 
 func main() {
-	/*mux := mux.NewRouter().StrictSlash(true)
-	//models.InitializeNetworks()
+	mux := mux.NewRouter().StrictSlash(true)
+	models.InitializeNetworks()
 	data.LoadFromDB()
 	mux.HandleFunc("/", AddressValidationHandler).Methods("POST")
 
-	http.ListenAndServe(":9000", mux)*/
-
-	val := band_validator.IsValidAddress("band1lrakj39k2ledcahdkqqq5ugjgzhz445ntg5rll")
-	println(val)
-
+	http.ListenAndServe(":9000", mux)
 }
